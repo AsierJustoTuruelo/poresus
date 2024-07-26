32ad2507bf24c581ea46b879279579e7c7423ac4
Asier Justo Turuelo <98945579+AsierJustoTuruelo@users.noreply.github.com>
2024-07-26 17:10:35 +0200

    Add files via upload

--- poresus.py
+++ poresus.py
@@ -1,22 +1,11 @@
 """
-Pruebas a realizar:
-
-1. Identificar el tipo de servidor que corre y su version. PUede ser en el header o habria que ver otros metodos.
-2. Identificar puertos abiertos en el servidor.
-3. Identificar si el html contiene mails, telefonos, direcciones
-4. Identificar si el html contiene direcciones bitcoin
-5. Identificar si el html contiene direcciones de redes sociales
-6. Identificar si el html contiene direcciones de google analytics
-7. Identificar si se pueden descargar imagenes, videos, pdfs, etc.
-
-"""
-"""
-
 instalar mmh3 --> pip install mmh3
 instalar mutagen --> pip install mutagen
 instalar stem --> pip install stem
 
 """
+
+import argparse
 import json
 from BitcoinAddressExtractorFunc.BitcoinAddressExtractor import BitcoinAddressExtractor
 from BruteForceScannerFunc.BruteForceScanner import BruteForceScanner
@@ -48,8 +37,7 @@ from AdvancedSqlInjectionScannerFunc.AdvancedSqlInjectionScanner import Advanced
 from InputValidatorFunc.InputValidator import InputValidator
 from XSSScannerFunc.XSSScanner import XSSScanner
 from DatabaseTypeScannerFunc.DatabaseTypeScanner import DatabaseTypeScanner
-import argparse
-import json
+
 
 # Adjust this to the path of the usernames and passwords files
 usernames_file = "./dics/usernames.txt"
